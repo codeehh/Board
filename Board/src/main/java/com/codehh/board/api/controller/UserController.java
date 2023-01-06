77a3cd9942c3a0d342b35254c77fdb8a6fad6381
노호준 <hh950131@gmail.com>
2023-01-06 17:40:03 +0900

    BE feat/user 로그아웃, 아이디 찾기, 비밀번호 찾기(초기화)

--- Board/src/main/java/com/codehh/board/api/controller/UserController.java
+++ Board/src/main/java/com/codehh/board/api/controller/UserController.java
@@ -2,20 +2,21 @@ package com.codehh.board.api.controller;
 
 import com.codehh.board.api.dto.user.request.JoinReq;
 import com.codehh.board.api.dto.user.request.LoginReq;
-import com.codehh.board.api.dto.user.response.JoinRes;
+import com.codehh.board.api.dto.user.response.LoginRes;
 import com.codehh.board.api.service.UserService;
 import com.codehh.board.common.exception.JoinFailureException;
 import com.codehh.board.common.exception.LoginFailureException;
+import com.codehh.board.db.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
@@ -81,31 +82,102 @@ public class UserController {
     @PostMapping("/users")
     public ResponseEntity<Object> join(@RequestBody JoinReq joinReq) {
         HttpStatus status = HttpStatus.OK;
-        JoinRes result = null;
         try {
-            result = userService.join(joinReq);
+            userService.join(joinReq);
         } catch (NoSuchAlgorithmException e) {
             status = HttpStatus.INTERNAL_SERVER_ERROR;
         } catch (JoinFailureException e) {
             status = HttpStatus.BAD_REQUEST;
         }
 
-        return ResponseEntity.status(status).body(result);
+        return ResponseEntity.status(status).build();
     }
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody LoginReq loginReq, HttpServletRequest req) {
         HttpStatus status = HttpStatus.OK;
-        log.info(loginReq.toString());
+        LoginRes result = null;
         try {
-            userService.login(loginReq, req);
+            result = userService.login(loginReq, req);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         } catch (LoginFailureException e) {
             throw new RuntimeException(e);
         }
 
-        return null;
+        return ResponseEntity.status(status).body(result);
     }
 
+    @GetMapping("/logout")
+    public ResponseEntity<Object> logout(HttpServletRequest req) {
+        HttpStatus status = HttpStatus.OK;
+        userService.logout(req);
+
+        return ResponseEntity.status(status).build();
+    }
+
+    @PostMapping("/find-id")
+    public ResponseEntity<Object> findId(@RequestBody HashMap<String, Object> payload) {
+        String email = (String) payload.get("email");
+        String authCode = (String) payload.get("auth_code");
+
+        HttpStatus status = HttpStatus.OK;
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("id", null);
+        //인증코드 매칭 확인
+        if (emailToAuthCode.get(email).equals(authCode)) {
+            List<String> ids = userService.findId(email);
+            if (ids != null) {
+                result.put("id", ids);
+            }
+        }
+
+        return ResponseEntity.status(status).body(result);
+    }
+
+    @PostMapping("/find-password")
+    public ResponseEntity<Object> findPassword(@RequestBody HashMap<String, Object> payload) {
+        String id = (String) payload.get("id");
+        String email = (String) payload.get("email");
+        String authCode = (String) payload.get("auth_code");
+
+        HttpStatus status = HttpStatus.OK;
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        boolean isMatch = false;
+
+        //인증코드 매칭 확인
+        if (emailToAuthCode.get(email).equals(authCode)) {
+            User user = userService.findPassword(id, email);
+            if (user != null) {
+                isMatch = true;
+            }
+        }
+        result.put("isMatch", isMatch);
+
+        return ResponseEntity.status(status).body(result);
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Object> resetPassword(@RequestBody HashMap<String, Object> payload) {
+        String id = (String) payload.get("id");
+        String password = (String) payload.get("password");
+
+        HttpStatus status = HttpStatus.OK;
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        boolean isReset = false;
+
+        //비밀번호 조건 검사
+        if (userService.passwordCheck(password)) {
+            try {
+                if (userService.resetPassword(id, password)) {
+                    isReset = true;
+                }
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        result.put("isReset", isReset);
+
+        return ResponseEntity.status(status).body(result);
+    }
 }
--- Board/src/main/java/com/codehh/board/api/service/UserService.java
+++ Board/src/main/java/com/codehh/board/api/service/UserService.java
@@ -2,13 +2,14 @@ package com.codehh.board.api.service;
 
 import com.codehh.board.api.dto.user.request.JoinReq;
 import com.codehh.board.api.dto.user.request.LoginReq;
-import com.codehh.board.api.dto.user.response.JoinRes;
+import com.codehh.board.api.dto.user.response.LoginRes;
 import com.codehh.board.common.exception.JoinFailureException;
 import com.codehh.board.common.exception.LoginFailureException;
+import com.codehh.board.db.entity.User;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
+import java.util.List;
 
 public interface UserService {
 
@@ -16,9 +17,19 @@ public interface UserService {
 
     public boolean nicknameCheck(String nickname);
 
+    public boolean passwordCheck(String password);
+
     public String sendEmail(String email);
 
-    public JoinRes join(JoinReq joinReq) throws NoSuchAlgorithmException, JoinFailureException;
+    public void join(JoinReq joinReq) throws NoSuchAlgorithmException, JoinFailureException;
 
-    public void login(LoginReq loginReq, HttpServletRequest req) throws NoSuchAlgorithmException, LoginFailureException;
+    public LoginRes login(LoginReq loginReq, HttpServletRequest req) throws NoSuchAlgorithmException, LoginFailureException;
+
+    public void logout(HttpServletRequest req);
+
+    public List<String> findId(String email);
+
+    public User findPassword(String id, String email);
+
+    public boolean resetPassword(String id, String password) throws NoSuchAlgorithmException;
 }
